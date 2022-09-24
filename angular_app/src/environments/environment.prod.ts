58fa72a73314756a15ce052ed3bc1fa6a1555fbf
AshithaKulal <ashithakadthala@gmail.com>
2022-09-24 16:58:18 +0530

    commit 24/9

--- angular_app/src/environments/environment.prod.ts
+++ angular_app/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   postsapi:"https://heroku-json-server-flipkart.herokuapp.com/posts",
   userapi:"https://heroku-json-server-flipkart.herokuapp.com/users",
-  cartapi:"https://heroku-json-server-flipkart.herokuapp.com/cart"
+  cartapi:"https://heroku-json-server-flipkart.herokuapp.com/cart",
+  commentapi:"https://heroku-json-server-flipkart.herokuapp.com/comments"
 
 };
